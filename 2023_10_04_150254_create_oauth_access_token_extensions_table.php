618ff2a770f274dd8d0c7ce8fba11b3068a9295a
Hamis Juma <hamisjuma1@gmail.com>
2023-10-05 12:41:50 +0300

    Update 2023_10_04_150254_create_oauth_access_token_extensions_table.php

--- 2023_10_04_150254_create_oauth_access_token_extensions_table.php
+++ 2023_10_04_150254_create_oauth_access_token_extensions_table.php
@@ -1,1 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOauthAccessTokenExtensionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth_access_token_extensions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('oauth_access_token_id', 100)->unique();
+            $table->morphs('owner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oauth_access_token_extensions');
+    }
+}
 
